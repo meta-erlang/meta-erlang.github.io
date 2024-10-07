d4664a14960c59b3437fd42de247c28c0050d036
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2024-10-06 23:04:52 -0300

    Fix broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/guides-quickstart/">
+            to="docs/master/guides-quickstart">
             Quickstart guide - 5min ⏱️
           </Link>
         </div>
